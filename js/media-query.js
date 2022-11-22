f98f29f1ba66f4a7d5d0da2421dfd87d0df4562e
unknown <SENA@PITAPRSGYFSP045.FormacionSena.RED>
2022-11-22 14:55:42 -0500

    22/11/2022

--- js/media-query.js
+++ js/media-query.js
@@ -1,5 +1,15 @@
 let barMenu = document.getElementById("barMenu");
+let mainMenu = document.getElementById("mainMenu");
 
 barMenu.addEventListener("click", function() {
-    alert("Funcionó")
+if (mainMenu.classList.contains("menu-bloque-oculto")) {
+    mainMenu.classList.add("menu-bloque-visible");
+    mainMenu.classList.remove("menu-bloque-oculto");
+}
+else{
+    mainMenu.classList.remove("menu-bloque-visible");
+    mainMenu.classList.add("menu-bloque-oculto");
+}
+
+
 });
